e30488add50aec8dc643a3a516cdbb909a146586
Felicia Kwame <kwamefelicia2005@gmail.com>
2024-08-30 10:26:09 +0300

    final commit

---
+++ functiontest.py
@@ -0,0 +1,7 @@
+def stylish_painter():
+    best_hairstyle = "Bob Ross"
+    return "Jean-Michel Basquiat"
+    return best_hairstyle # a function ends as it encounters the return keyword
+    print(best_hairstyle)
+
+stylish_painter()
--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+greet_programmer()
+
 
 def greet(name):
-    pass
+    print("Hello,"+name+"!")
+greet("Felicia")
 
 def greet_with_default(name="programmer"):
-    pass
+    print("Hello, "+name+"!")
+greet_with_default()
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+add(5,3)
 
 def halve(number):
-    pass
+    return number / 2
+halve(8)
